2ae95a0fecc222120051e8191baf1e6dbb2a7c3a
Amer-Ahmed <amer.ahmed@efreshli.com>
2024-05-22 18:01:46 +0300

    replace employee id with the unique identifier

---
+++ database/1378_ReplaceEmployeeIDWithUniqueIdentifier.sql
@@ -0,0 +1,9 @@
+# problemUrl = https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/
+
+SELECT
+    EmployeeUNI.unique_id,
+    Employees.name
+FROM
+    Employees
+LEFT JOIN
+    EmployeeUNI ON Employees.id = EmployeeUNI.id;
